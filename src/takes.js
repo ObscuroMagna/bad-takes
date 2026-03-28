ce69b45a55f5f59a44f7f1027ad466f8325847c8
Obscuromagna <725778+ObscuroMagna@users.noreply.github.com>
2026-03-28 15:41:40 -0400

    Starting my list

--- src/takes.js
+++ src/takes.js
@@ -1,19 +1,13 @@
 const takes = [
-  "The Star Wars prequels are better than the originals",
-  "Pineapple on pizza is a crime against humanity",
+  "The federal government is a well oiled machine",
   "Hot dogs are sandwiches",
-  "Water is wet",
-  "The book is NOT always better than the movie",
   "Cereal is soup",
-  "Ketchup on steak is perfectly fine",
+  "Magnus did nothing wrong",
   "Die Hard is not a Christmas movie",
   "GIF is pronounced 'jif'",
   "The last season of Game of Thrones was actually good",
   "Nickelback is genuinely talented",
-  "Crocs are fashionable footwear",
-  "Batman could beat Superman in a fair fight",
   "Candy corn is delicious",
-  "The toilet paper goes UNDER, not over",
 ];
 
 export default takes;
